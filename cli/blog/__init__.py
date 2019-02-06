4194741ba8eca738886cc9d61834144565657222
Ardika Bagus S <ardikabs@gmail.com>
2019-02-06 13:22:17 +0700

    Update __init__.py
    change git url

--- cli/blog/__init__.py
+++ cli/blog/__init__.py
@@ -3,4 +3,4 @@
 __version__ = "0.1.0"
 __author__ = "Ardika Bagus Saputro"
 __email__ = "ardikabs@gmail.com"
-__url__ = "https://github.com/ardikabs/wordpress-blog-automation"
+__url__ = "https://github.com/blog-automation-terraform-ansible-stack"
